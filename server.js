da444507a74ff5ff5d5ab9a99eb21af20409e630
Jordan Tranchina <Jordan12522@gmail.com>
2026-02-16 22:41:03 -0600

    chore: Ignore Vercel files and use an absolute path for serving static assets.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/inventory", async (req, res) => {
   try {
